092496de132469b1ef3b7a558697724ab2914f77
bIropka <biropka@gmail.com>
2016-11-08 16:50:06 +0200

    v 0.1

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -21,6 +21,10 @@ $(document).ready(function () {
     /******************************
      ******* clicks
      ******************************/
+
+    $('.burger').click(function () {
+        $(this).siblings('ul').slideToggle();
+    });
     
 
     /*******************************
@@ -45,59 +49,6 @@ $(document).ready(function () {
         nextArrow: '.slider-banner-right',
         autoplay: true,
         autoplaySpeed: 6000
-    });
-
-    /*******************************
-     ******* digits  script ********
-     ******************************/
-    var show = true;
-    var countbox = ".digits";
-    $(window).on("scroll load resize", function() {
-
-        if (!show) return false;
-
-        var w_top = $(window).scrollTop();
-        var e_top = $(countbox).offset().top;
-
-        var w_height = $(window).height();
-        var d_height = $(document).height();
-
-        var e_height = $(countbox).outerHeight();
-
-        if (w_top + w_height * 0.8 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
-
-            var space_separator_number_step = $.animateNumber.numberStepFactories.separator(' ');
-
-            $('#our-years').animateNumber(
-                {
-                    number: 6,
-                    numberStep: space_separator_number_step
-                }, 1000
-            );
-
-            $('#our-logisticians').animateNumber(
-                {
-                    number: 26,
-                    numberStep: space_separator_number_step
-                }, 1300
-            );
-
-            $('#our-clients').animateNumber(
-                {
-                    number: 248,
-                    numberStep: space_separator_number_step
-                }, 2000
-            );
-
-            $('#our-offices').animateNumber(
-                {
-                    number: 8,
-                    numberStep: space_separator_number_step
-                }, 1000
-            );
-
-            show = false;
-        }
     });
 
 });
