b6768f91ae1c75cab0406bd14d662058f5c81c1d
bIropka <biropka@gmail.com>
2016-11-08 22:35:29 +0200

    v 0.2

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -3,18 +3,35 @@ $(document).ready(function () {
     /******************************
      ******* init scripts
      ******************************/
+    
+    setTimeout(function() {
+        $('.slider-wrapper').animate({opacity: 1}, 500);
+    }, 500);
 
-    if ($(window).width() < '992'){
-
+    if ($(window).width() > '1000'){
+        $('nav ul').css('display', 'block');
     } else {
+        $('nav ul').css('display', 'none');
+    }
+    if ($(window).width() > '800') {
+        $('.account-info').css('display', 'block');
+    } else {
+        $('.account-info').css('display', 'none');
+        $('.account-mobile').find('i').toggleClass('fa-arrow-up fa-arrow-down');
 
     }
 
     $(window).resize(function(){
-        if ($(window).width() < '992'){
-
+        if ($(window).width() > '1000'){
+            $('nav ul').css('display', 'block');
         } else {
-            
+            $('nav ul').css('display', 'none');
+        }
+        if ($(window).width() > '800') {
+            $('.account-info').css('display', 'block');
+        } else {
+            $('.account-info').css('display', 'none');
+            $('.account-mobile').find('i').toggleClass('fa-arrow-up fa-arrow-down');
         }
     });
 
@@ -23,12 +40,17 @@ $(document).ready(function () {
      ******************************/
 
     $('.burger').click(function () {
-        $(this).siblings('ul').slideToggle();
+        $(this).siblings('ul').slideToggle(200);
+    });
+
+    $('.account-mobile').click(function () {
+        $(this).find('i').toggleClass('fa-arrow-up fa-arrow-down');
+        $(this).siblings('.account-info').slideToggle(200);
     });
     
 
     /*******************************
-     ******* slider scripts ********
+     ******* slider scripts 
      ******************************/
 
     $('.slider-winner').slick({
@@ -50,6 +72,48 @@ $(document).ready(function () {
         autoplay: true,
         autoplaySpeed: 6000
     });
+
+    /*******************************
+     ******* modals
+     ******************************/
+
+    $('.sign-in').click(function () {
+        $('.window-sign-in').fadeIn();
+        if ($(window).width() < '801') {
+            $('.account-info').css('display', 'none');
+            $('.account-mobile').find('i').toggleClass('fa-arrow-up fa-arrow-down');
+        }
+    });
+
+    $('.forget-password').click(function () {
+        $('.window-forget-password').fadeIn();
+        $('.window-sign-in').fadeOut();
+    });
+
+    $('.sign-up').click(function () {
+        $('.window-sign-up').fadeIn();
+        if ($(window).width() < '801') {
+            $('.account-info').css('display', 'none');
+            $('.account-mobile').find('i').toggleClass('fa-arrow-up fa-arrow-down');
+        }
+    });
+
+    $('.withdraw-money').click(function () {
+        $('.window-withdraw').fadeIn();
+        if ($(window).width() < '801') {
+            $('.account-info').css('display', 'none');
+            $('.account-mobile').find('i').toggleClass('fa-arrow-up fa-arrow-down');
+        }
+    });
+
+
+
+    $('.window').click(function (event) {
+        $target = $(event.target);
+        if (!$target.closest($('form')).length) $('.window').fadeOut();
+        if ($target.hasClass('close-window')) $('.window').fadeOut();
+    });
+
 
 });
 
